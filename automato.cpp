1b6f04ebbf7f77fe586b740a927c2d6c9606a06b
joaovdamaceno <damacenovitorj@gmail.com>
2025-07-15 16:27:12 -0300

    string processing function added

--- automato.cpp
+++ automato.cpp
@@ -66,6 +66,44 @@ void Automato::lerTransicoes(FILE *f) {
     }
 }
 
+void Automato::processaCadeia(string cadeia, int iterador) {
+    cout << "[q" << iterador << ']' << cadeia << endl;
+
+    if (cadeia.empty() || cadeia[0] == '@') {
+        for (int i = 0; i < this->estadosFinais.size(); i++) {
+            if (iterador == this->estadosFinais[i]) {
+                cout << "ACEITA" << endl;
+                return;
+            }
+        }
+
+        cout << "REJEITA" << endl;
+        return;
+    }
+
+    if (this->transicoes[iterador].empty()) {
+        cout << "REJEITA" << endl;
+        return;
+    }
+
+    if (this->alfabeto.empty()) {
+        cout << "REJEITA" << endl;
+        return;
+    }
+
+    for (int i = 0; i < this->transicoes[iterador].size(); i++) {
+        if (this->transicoes[iterador][i].second == cadeia[0]) {
+            char letra = cadeia[0];
+            cadeia.erase(0, 1); 
+            processaCadeia(cadeia, this->transicoes[iterador][i].first);
+            return;
+        }
+    }
+
+    cout << "REJEITA" << endl;
+    return;
+}
+
 Automato::Automato() {
     printf("Digite o nome do arquivo: ");
     
